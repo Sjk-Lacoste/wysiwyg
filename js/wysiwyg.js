c68acba6653420a06610ff3079470bcc44838747
Sjk-Lacoste <tmohlatlole@gmail.com>
2019-10-28 21:59:24 +0200

    Added functionality for the default buttons

--- js/wysiwyg.js
+++ js/wysiwyg.js
@@ -20,8 +20,8 @@ let textEditor = new (function () {
         let defaultElements = [
             {command: 'bold', type: 'button', innerHTML: 'Bold'},
             {command: 'italic', type: 'button', innerHTML: 'Italic'},
-            {command: 'uderline', type: 'button', innerHTML: 'Underline'},
-            {command: 'strikeThrough', type: 'button', innerHTML: 'Strike'},
+            {command: 'underline', type: 'button', innerHTML: 'Underline'},
+            {command: 'strikeThrough', type: 'button', innerHTML: 'Strike'}
         ];
 
         // Container
@@ -41,28 +41,51 @@ let textEditor = new (function () {
         // Make the iframe editable in the browser
         contentEditable.contentDocument.designMode = "on";
 
-        //  Loop through the buttons
-        let el = 0;
-        for(el in defaultElements) {
-            let thisElement;
+
+        for(let idx = 0; idx < defaultElements.length; idx++) {
+            // let thisElement;
             
             // Create element
-            let element = document.createElement(defaultElements[el].type);
+            let element = document.createElement(defaultElements[idx].type);
+            element.appendBefore(document.getElementById('myRichTextEditorField'));
             
-            if (el > 0) {
-                thisElement = element;
-            }
-            
-            if (el > 0) {
-                element.appendAfter(thisElement);
-            } else {
-                element.appendBefore(contentEditable);
-            }
+            element.setAttribute('title', defaultElements[idx].command);
 
-            element.innerHTML = defaultElements[el].innerHTML;
-            element.style.margin = '0 5px 5px 0';
+            element.innerHTML = defaultElements[idx].innerHTML;
+            element.setAttribute('type', defaultElements[idx].type);
+            element.style.margin = '0 2px 5px 0';
+            element.style.padding = '5px 5px 5px 5px';
+            element.style.border = 'none';
+            element.style.background = "#d3d3d3";
+
+            let command;
+            let argument = null;
+
+            // if its button
+            if (defaultElements[idx].type.indexOf('button') !== -1) {
+                let showCode = false;
+                let isPrompt = false;
+
+                element.onclick = function () {
+                    command = this.getAttribute('title');
+                    if (command == 'viewSourceCode') {
+                        // view source code of the html tags
+                        showCode = execViewSourceCommand(element, contentEditable, showCode);
+                    } else {
+                        // switch (command) {
+                            
+                        // }
+
+                        if ((argument !== null && isPrompt) || !isPrompt)
+                            myRichTextEditorField.document.execCommand(command, false, argument);
+                    }
+                };
+            } else {
+                
+            }
         }
     };
+    
 
     Element.prototype.appendBefore = function(element) {
         element.parentNode.insertBefore(this, element);
@@ -71,4 +94,11 @@ let textEditor = new (function () {
     Element.prototype.appendAfter = function (element) {
         element.parentNode.insertBefore(this, element.nextSibling);
     }, false;
+
+    let loadIcons = function(){
+        let icons = document.createElement('link');
+        icons.setAttribute('href', '');
+        icons.setAttribute('rel', 'stylesheet');
+        icons.setAttribute('type', 'text/css');
+    };
 });
